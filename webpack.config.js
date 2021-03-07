83e7fb57531f940d73a365c597e4673279960629
SHELDON SMALL <sheldonsmall.io@gmail.com>
2021-03-06 22:13:08 -0500

    Add webpack configuration (#1)
    * move files to src directory
    
    * install webpack
    
    * remove node modules
    
    * add web pack and gitignore

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require( 'path' );
+
+module.exports = {
+
+    devtool: 'source-map',
+
+    // bundling mode
+    mode: 'production',
+
+    // entry files
+    entry: './src/main.ts',
+
+    // output bundles (location)
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'main.js',
+    },
+
+    // file resolutions
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+
+      // loaders
+      module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    }
+                },
+                exclude: /node_modules/,
+            }
+        ]
+    },
+
+    // plugins
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(), // run TSC on a separate thread
+    ],
+
+    // set watch mode to `true`
+    watch: true
+};
