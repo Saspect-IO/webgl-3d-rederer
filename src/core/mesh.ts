a61a3469f8c38bbe6997003cd89aa0c04356de16
Sheldon Small <sheldonsmall.io@gmail.com>
2021-03-07 15:55:21 -0500

    fix merge conflicts

--- src/core/mesh.ts
+++ src/core/mesh.ts
@@ -2,28 +2,27 @@ import Geometry from './geometry';
 import Texture from './texture';
 import Transformation from './transformation';
 import Vbuffer from './vbuffer';
-import ShaderProgram  from './shaderProgram';
+import ShaderProgram from './shaderProgram';
 
 export default class Mesh {
 
-  constructor(gl: WebGLRenderingContext, geometry:any, texture:any) {
-      this.vertexCount = geometry.vertexCount();
-      this.positions = new Vbuffer(gl, geometry.positions(), this.vertexCount);
-      this.normals = new Vbuffer(gl, geometry.normals(), this.vertexCount);
-      this.uvs = new Vbuffer(gl, geometry.uvs(), this.vertexCount);
-      this.texture = texture;
-      this.vertexCount = this.vertexCount;
-      this.position = new Transformation();
-      this.gl = gl;
+  constructor(gl: WebGLRenderingContext, geometry: Geometry, texture: Texture) {
+    this.vertexCount = geometry.vertexCount();
+    this.positions = new Vbuffer(gl, geometry.positions(), this.vertexCount);
+    this.normals = new Vbuffer(gl, geometry.normals(), this.vertexCount);
+    this.uvs = new Vbuffer(gl, geometry.uvs(), this.vertexCount);
+    this.texture = texture;
+    this.position = new Transformation();
+    this.gl = gl;
   }
 
-  vertexCount:number;
-  positions:any;
-  normals:any;
-  uvs:any;
-  texture:any;
-  position:any;
-  gl: WebGLRenderingContext = null;
+  vertexCount: number;
+  positions: Vbuffer;
+  normals: Vbuffer;
+  uvs: Vbuffer;
+  texture: Texture;
+  position: Transformation;
+  gl: WebGLRenderingContext | null = null;
 
 
   destroy() {
@@ -33,20 +32,21 @@ export default class Mesh {
   }
 
   drawMesh(shaderProgram: ShaderProgram) {
-    this.positions.bindToAttribute(shaderProgram.position);
-    this.normals.bindToAttribute(shaderProgram.normal);
-    this.uvs.bindToAttribute(shaderProgram.uv);
-    this.position.sendToGpu(this.gl, shaderProgram.model);
-    this.texture.use(shaderProgram.diffuse, 0);
-    this.gl.drawArrays(this.gl.TRIANGLES, 0, this.vertexCount);
+    this.positions.bindToAttribute(shaderProgram.positionIndex);
+    this.normals.bindToAttribute(shaderProgram.normalIndex);
+    this.uvs.bindToAttribute(shaderProgram.uvIndex);
+    this.position.sendToGpu(this.gl as WebGLRenderingContext, shaderProgram.model as WebGLUniformLocation);
+    this.texture.useTexture(shaderProgram.diffuse as WebGLUniformLocation, 0);
+    this.gl?.drawArrays(this.gl.TRIANGLES, 0, this.vertexCount);
   }
 
-  static async loadMesh(gl: WebGLRenderingContext, modelPath: string, texturePath: string) {
-    const geometry = Geometry.loadOBJ(modelPath);
-    const texture = Texture.loadTexture(gl, texturePath);
-    return Promise.all([geometry, texture]).then(function (params) {
-      return new Mesh(gl, params[0], params[1]);
-    })
+  static async loadMesh(gl: WebGLRenderingContext, objSrc: string, textureSrc: string) {
+    const geometry = await Geometry.loadOBJ(objSrc);
+    const texture = await Texture.loadTexture(gl, textureSrc);
+    const [geometryData, geometryTexture] = await Promise.all([geometry, texture]);
+    const mesh = new Mesh(gl, geometryData, geometryTexture as any);
+
+    return mesh;
   }
 
 }
--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -1,22 +1,24 @@
-import { ProgramEntry } from '../modules';
-import ShaderProgram  from './shaderProgram';
-import Camera  from './camera';
-import Light  from './light';
+import {
+    ProgramEntry
+} from '../modules';
+import ShaderProgram from './shaderProgram';
+import Camera from './camera';
+import Light from './light';
 import Mesh from './mesh';
 
 export default class Renderer {
-    constructor(canvas:HTMLCanvasElement ){
+    constructor(canvas: HTMLCanvasElement) {
         this.gl = canvas.getContext(ProgramEntry.WEBGL_CONTEXT);
         this.gl?.enable(this.gl.DEPTH_TEST);
         this.shaderProgram = null;
     }
 
-    gl:WebGLRenderingContext|null = null;
-    shaderProgram:ShaderProgram|null = null;
+    gl: WebGLRenderingContext | null = null;
+    shaderProgram: ShaderProgram | null = null;
     rgb_32_bit = 255;
     alpha = 1;
 
-    setClearColor(red:number, green:number, blue:number, alpha:number = 1){
+    setClearColor(red: number, green: number, blue: number, alpha: number = 1) {
         this.gl?.clearColor(red / this.rgb_32_bit, green / this.rgb_32_bit, blue / this.rgb_32_bit, alpha);
     }
 
@@ -24,11 +26,11 @@ export default class Renderer {
         return this.gl;
     }
 
-    setShaderProgram(shaderProgram: ShaderProgram){
+    setShaderProgram(shaderProgram: ShaderProgram) {
         this.shaderProgram = shaderProgram;
     }
-    
-    render(camera: Camera, light: Light, objects: Array<Mesh>){
+
+    render(camera: Camera, light: Light, objects: Array < Mesh > ) {
         this.gl?.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT)
         const shaderProgram = this.shaderProgram;
 
@@ -39,7 +41,7 @@ export default class Renderer {
         shaderProgram.useShaderProgram();
         light.useLight(shaderProgram);
         camera.useCamera(shaderProgram);
-        objects.forEach((mesh)=>mesh.drawMesh(shaderProgram));
+        objects.forEach((mesh) => mesh.drawMesh(shaderProgram));
     }
-    
+
 }
