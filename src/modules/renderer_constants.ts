ad625b570e8d4739c9aa30568aa3eef4e193efc4
Sheldon Small <sheldonsmall.io@gmail.com>
2021-03-20 20:55:00 -0500

    update asset location

--- src/modules/renderer_constants.ts
+++ src/modules/renderer_constants.ts
@@ -5,8 +5,8 @@ export enum ProgramEntrySettings {
     WEBGL_CONTEXT_WEBKIT = 'webkit-3d',
     WEBGL_CONTEXT_MOZ = 'moz-webgl',
     WEBGL_CONTEXT_ERROR_MESSAGE = 'Could not initialise WebGL',
-    PATH_ASSETS_SPHERE = '/assets/Mandalorian.obj',
-    PATH_ASSETS_DIFFUSE = '/assets/diffuse.png',
+    PATH_ASSETS_SPHERE = '/assets/resources/Mandalorian.obj',
+    PATH_ASSETS_DIFFUSE = '/assets/resources/textures/diffuse.png',
     PATH_SHADE_VERTEX = '/shaders/basic.vert',
     PATH_SHADE_FRAGMENT = '/shaders/basic.frag',
 }
