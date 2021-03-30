e126922197d41820baa83291cdc8268ba8c54506
SHELDON SMALL <sheldonsmall.io@gmail.com>
2021-03-30 13:44:44 -0500

    Add webpack configuration (#3)
    * move files to src directory
    
    * install webpack
    
    * remove node modules
    
    * add web pack and gitignore
    
    * updated ts entry point
    
    * removed plugin
    
    * update ts config
    
    * moved assets
    
    * update webpack and index.html
    
    * updated git to ignore dist directory
    
    * update structs
    
    * types code clean up
    
    * code types clean up
    
    * fix shader program fetch state
    
    * fix geometry object return state
    
    * code clean up
    
    * code clean up and added data types
    
    * fix merge conflicts
    
    * added html webpack plugin

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,30 @@ import Camera  from './core/camera';
 import Light  from './core/light';
 import Mesh from './core/mesh';
 import Transformation  from './core/transformation';
-import { ProgramEntrySettings } from './modules';
+import {
+    ProgramEntry
+} from './modules/index';
 
 
-const canvas = document.getElementById(ProgramEntrySettings.WEBGL_CANVAS_ID) as HTMLCanvasElement;
-const renderer = new Renderer(canvas);
+const renderer = new Renderer(document.getElementById(ProgramEntry.WEBGL_CANVAS_ID) as HTMLCanvasElement)
 renderer.setClearColor(0.0, 0.0, 0.0, 1.0);
-const glContext = renderer.getContext() as WebGLRenderingContext;
+const gl = renderer.getContext() as WebGLRenderingContext;
 
-const model: Mesh[] = [];
+const meshArray: Mesh[] = [];
 
-Mesh.loadMesh(glContext, ProgramEntrySettings.PATH_ASSETS_SPHERE, ProgramEntrySettings.PATH_ASSETS_DIFFUSE)
-    .then((mesh) => model.push(mesh));
+Mesh.loadMesh(gl, ProgramEntry.PATH_ASSETS_SPHERE, ProgramEntry.PATH_ASSETS_DIFFUSE)
+    .then((mesh) => meshArray.push(mesh));
 
-ShaderProgram.initShaderProgram(glContext, ProgramEntrySettings.PATH_SHADE_VERTEX, ProgramEntrySettings.PATH_SHADE_FRAGMENT)
+ShaderProgram.initShaderProgram(gl, ProgramEntry.PATH_SHADE_VERTEX, ProgramEntry.PATH_SHADE_FRAGMENT)
     .then(shaderProgram => renderer.setShaderProgram(shaderProgram));
 
 
-const camera = new Camera(); 
-camera.setOrthographic(16, 8, 12);
-camera.position = (camera.position as Transformation).scale(0.18,0.18,0.18).translate(0,50,0);
-const light = new Light(-1,-1,-1);
+const camera = new Camera(); camera.setOrthographic(16, 10, 10);
+const light = new Light();
 
 const loop = () => {
-    renderer.render(camera, light, model);
+    renderer.render(camera, light, meshArray);
+    camera.position = (camera.position as Transformation).rotateY(Math.PI / ProgramEntry.CAMERA_ANGLE_DIVISION);
     requestAnimationFrame(loop);
 }
 
