63743202caad59abe5259977eb24ae0e5b7f67b5
Sheldon Small <sheldonsmall.io@gmail.com>
2021-04-01 17:29:39 -0500

    update renederer

--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -1,55 +1,35 @@
-import {
-    ProgramEntrySettings
-} from '../modules';
 import ShaderProgram from './shaderProgram';
-import Camera from './camera';
+import {Camera, CameraController} from './camera';
 import Light from './light';
 import Mesh from './mesh';
-import Primitives from './primitives';
+import { GridAxisShader } from './shaderExtend';
+import { GridAxis } from './primitives';
+import Modal from './modal';
+import GLContext from './glContext';
 
 export default class Renderer {
-    constructor(canvas: HTMLCanvasElement) {
-        ([
-            ProgramEntrySettings.WEBGL_CONTEXT,
-            ProgramEntrySettings.WEBGL_CONTEXT_EXPERIMENTAL,
-            ProgramEntrySettings.WEBGL_CONTEXT_WEBKIT,
-            ProgramEntrySettings.WEBGL_CONTEXT_MOZ
-        ]).some(option => this.gl = canvas.getContext(option) as WebGLRenderingContext);
 
-        this.gl ?? alert(ProgramEntrySettings.WEBGL_CONTEXT_ERROR_MESSAGE);
-        this.gl?.enable(this.gl.CULL_FACE);
-        this.gl?.enable(this.gl.DEPTH_TEST);
+    constructor(gl: WebGLRenderingContext) {
+        gl?.enable(gl.CULL_FACE);
+        gl?.enable(gl.DEPTH_TEST);
     }
 
-    gl: WebGLRenderingContext | null = null;
     shaderProgram: ShaderProgram | null = null;
     rgb_32_bit = 255;
     alpha = 1;
-
-    setClearColor(red: number, green: number, blue: number, alpha: number = 1) {
-        this.gl?.clearColor(red / this.rgb_32_bit, green / this.rgb_32_bit, blue / this.rgb_32_bit, alpha);
-    }
-
-    getContext() {
-        return this.gl;
-    }
 
     setShaderProgram(shaderProgram: ShaderProgram) {
         this.shaderProgram = shaderProgram;
     }
 
-    render(camera: Camera, light: Light, model: Mesh[]) {
-        this.gl?.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT)
-        const shaderProgram = this.shaderProgram;
+    render(glContext:GLContext, camera: Camera, controls:CameraController, gGridShader:GridAxisShader, gGridModal:Modal, light: Light, model: Mesh[]) {
+        
+        camera.updateViewMatrix();
+        glContext.fClear();
 
-        if (!shaderProgram) {
-            return;
-        }
-
-        shaderProgram.useShaderProgram();
-        light.useLight(shaderProgram);
-        camera.useCamera(shaderProgram);
-        model.forEach((mesh) => mesh.drawMesh(shaderProgram));
+        gGridShader.activate()
+            .setCameraMatrix(camera.viewMatrix)
+            .renderModal(gGridModal.preRender());
     }
 
 }
