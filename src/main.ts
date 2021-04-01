91b870bc53f6dacb35baff41a2843be17aa2e692
Sheldon Small <sheldonsmall.io@gmail.com>
2021-04-01 17:29:52 -0500

    update program entry

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,40 @@
 import Renderer from './core/renderer';
 import ShaderProgram  from './core/shaderProgram';
-import Camera  from './core/camera';
+import {Camera, CameraController}  from './core/camera';
 import Light  from './core/light';
 import Mesh from './core/mesh';
-import Controls from './core/controls';
-import { CameraSettings, ProgramEntrySettings } from './modules';
-import { degToRad } from './modules';
-import Transformation from './core/transformation';
-import Primitives from './core/primitives';
-
+import { ProgramEntrySettings } from './modules';
+import { GridAxisShader } from './core/shaderExtend';
+import GLContext from './core/glContext';
+import { GridAxis } from './core/primitives';
 
 const model: Mesh[] = [];
-const primitives: Primitives[] = [];
-const canvas = document.getElementById(ProgramEntrySettings.WEBGL_CANVAS_ID) as HTMLCanvasElement;
 
-const renderer = new Renderer(canvas);
-const camera = new Camera(); 
+const glContext = new GLContext(ProgramEntrySettings.WEBGL_CANVAS_ID).fFitScreen(0.95,0.9).fClear();
+
+const camera = new Camera(glContext.gl as WebGLRenderingContext); 
+camera.transform.position.set(0,1,3);
+const controls = new CameraController(glContext.gl as WebGLRenderingContext, camera);
+
+//Setup Grid
+const gGridShader = new GridAxisShader(glContext.gl as WebGLRenderingContext, camera.projection);
+const gGridModal = GridAxis.createModal(glContext, true);
+
+const renderer = new Renderer(glContext.gl as WebGLRenderingContext);
+
 const light = new Light(-1,-1,-1);
-const controls = new Controls(canvas, camera);
 
-const cameraData = {
-    fieldOfView: degToRad(CameraSettings.FIELD_OF_VIEW),
-} 
-
-renderer.setClearColor(0.0, 0.0, 0.0, 1.0);
-const gl = renderer.getContext() as WebGLRenderingContext;
-const aspect = (gl.canvas.width / gl.canvas.height);
-
-// Primitives.loadPrimitives(gl)
-//     .then((data) => primitives.push(data));
-
-Mesh.loadMesh(gl, ProgramEntrySettings.PATH_ASSETS_SPHERE, ProgramEntrySettings.PATH_ASSETS_DIFFUSE)
-    .then((mesh) => model.push(mesh));
+// Mesh.loadMesh(glContext.gl as WebGLRenderingContext, ProgramEntrySettings.PATH_ASSETS_SPHERE, ProgramEntrySettings.PATH_ASSETS_DIFFUSE)
+//     .then((mesh) => model.push(mesh));
 
 
-ShaderProgram.initShaderProgram(gl, ProgramEntrySettings.PATH_SHADE_VERTEX, ProgramEntrySettings.PATH_SHADE_FRAGMENT)
-    .then(shaderProgram => renderer.setShaderProgram(shaderProgram));
+// ShaderProgram.initShaderProgram(glContext.gl as WebGLRenderingContext, ProgramEntrySettings.PATH_SHADE_VERTEX, ProgramEntrySettings.PATH_SHADE_FRAGMENT)
+//     .then(shaderProgram => renderer.setShaderProgram(shaderProgram));
 
-camera.setOrthographic(
-    CameraSettings.SCREEN_LEFT, 
-    gl.canvas.clientWidth,
-    CameraSettings.SCREEN_TOP, 
-    gl.canvas.clientHeight, 
-    CameraSettings.ORTHO_NEAR, 
-    CameraSettings.ORTHO_FAR
-);
-
-// camera.setPerspective(cameraData.fieldOfView, aspect, CameraSettings.NEAR_PLANE, CameraSettings.FAR_PLANE);
 
 const loop = () => {
-    renderer.render(camera, light, model);
-    // controls.updateFieldOfView(cameraData.fieldOfView);
-    camera.position = controls.zoom(camera);
+    renderer.render(glContext, camera, controls, gGridShader, gGridModal,  light, model);
+
     requestAnimationFrame(loop);
 }
 loop();
