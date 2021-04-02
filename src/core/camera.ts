140c7bee1450860900b6fb31d3f6aa32bfdbbeab
Sheldon Small <sheldonsmall.io@gmail.com>
2021-04-02 16:47:26 -0500

    add typing

--- src/core/camera.ts
+++ src/core/camera.ts
@@ -102,8 +102,8 @@ class CameraController {
     this.prevX = 0; //Previous X,Y position on mouse move
     this.prevY = 0;
 
-		this.onUpHandler = function(e){ self.onMouseUp(e); };		//Cache func reference that gets bound and unbound a lot
-		this.onMoveHandler = function(e){ self.onMouseMove(e); }
+		this.onUpHandler = function(e:MouseEvent){ self.onMouseUp(e); };		//Cache func reference that gets bound and unbound a lot
+		this.onMoveHandler = function(e: MouseEvent){ self.onMouseMove(e); }
 
 		this.canvas.addEventListener("mousedown",function(e){ self.onMouseDown(e); });		//Initializes the up and move events
 		this.canvas.addEventListener("mousewheel", function(e){ self.onMouseWheel(e); });	//Handles zoom/forward movement
