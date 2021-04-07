1d837d0beb05306a132f177ddcdc9f67d7282a8d
Sheldon Small <sheldonsmall.io@gmail.com>
2021-04-06 21:52:32 -0500

    update texture and enable blending

--- src/core/texture.ts
+++ src/core/texture.ts
@@ -4,13 +4,14 @@ export default class Texture {
         this.createdTexture = gl.createTexture() as WebGLTexture;
         // Set the newly created texture context as active texture
         gl.bindTexture(gl.TEXTURE_2D, this.createdTexture);
-        // Set texture parameters, and pass the image that the texture is based on
+        // Set texture parameters, and pass the image that the texture is based on to the gpu
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         // Set filtering methods
         // Very often shaders will query the texture value between pixels,
         // and this is instructing how that value shall be calculated
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.generateMipmap(gl.TEXTURE_2D);
 
         this.gl = gl;
     }
@@ -23,14 +24,12 @@ export default class Texture {
         const gl = this.gl as WebGLRenderingContext;
         // We can bind multiple textures, and here we pick which of the bindings
         // we're setting right now
-        gl.activeTexture(gl[`${this.TEXTURE_KEY}${binding}`])
+        gl.activeTexture(gl[`${this.TEXTURE_KEY}${binding}`]);
         // After picking the binding, we set the texture
-        gl.bindTexture(gl.TEXTURE_2D, this.createdTexture)
+        gl.bindTexture(gl.TEXTURE_2D, this.createdTexture);
         // Finally, we pass to the uniform the binding ID we've used
-        gl.uniform1i(uniform, binding)
-        // The previous 3 lines are equivalent to:
-        // texture[i] = this.createdTexture
-        // uniform = i
+        gl.uniform1i(uniform, binding);
+        gl.bindTexture(gl.TEXTURE_2D, null);
     }
 
     static loadTexture(gl: WebGLRenderingContext, url: string): Promise < unknown > {
