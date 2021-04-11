9f7b9d4af0cdb7c7a75ea6cadcaa2cd8b035cbb1
Sheldon Small <sheldonsmall.io@gmail.com>
2021-04-10 23:00:41 -0500

    code clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,60 @@
-const path = require( 'path' );
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-    devtool: 'source-map',
+  devtool: 'source-map',
 
-    // bundling mode
-    mode: 'production',
+  // bundling mode
+  mode: 'production',
 
-    // entry files
-    entry: './src/main.ts',
+  // entry files
+  entry: './src/main.ts',
 
-    // output bundles (location)
-    output: {
-        path: path.resolve( __dirname, 'dist' ),
-        filename: 'bundle.js',
+  // output bundles (location)
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  // file resolutions
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
     },
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    modules: [
+      ROOT,
+      'node_modules'
+    ]
+  },
 
-    // file resolutions
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src/')
+  // loaders
+  module: {
+    rules: [{
+        test: /\.tsx?/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          }
+        },
+        exclude: /node_modules/,
       },
-      extensions: ['.ts', '.tsx', '.js', '.json'],
-      modules: [
-        ROOT,
-        'node_modules'
-      ]
-    },
-
-      // loaders
-      module: {
-        rules: [
-            {
-                test: /\.tsx?/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        transpileOnly: true,
-                    }
-                },
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'assets/resources',
-              },
-              {
-                test: /\.(woff|woff2|eot|ttf|otf|obj)$/i,
-                type: 'assets/resources',
-              },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: './src/index.html'
-        })
-      ],
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'assets/resources',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|obj)$/i,
+        type: 'assets/resources',
+      },
+    ]
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
 };
