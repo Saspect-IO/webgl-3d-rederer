9463ad21d77336f5bb89b250d7361e5a207b8ace
Sheldon Small <sheldonsmall.io@gmail.com>
2021-07-24 12:47:58 -0500

    remove unused data

--- src/modules/global_constants.ts
+++ src/modules/global_constants.ts
@@ -11,10 +11,6 @@ export enum ProgramEntrySettings {
     WEBGL_DEPTH_TEXTUR_ERROR_MESSAGE = 'need WEBGL_depth_texture',
     PATH_ASSETS_OBJ = '/webgl-3d-renderer/assets/resources/yoshi/source/yoshi.obj',
     PATH_ASSETS_TEXTURE = '/webgl-3d-renderer/assets/resources/yoshi/textures/yoshi.png',
-    PATH_SHADE_VERTEX = '/shaders/basic.vert',
-    PATH_SHADE_FRAGMENT = '/shaders/basic.frag',
-    PRIMITIVE_SHADER_VERTEX = '/shaders/primitive.vert',
-    PRIMITIVE_SHADER_FRAGMENT = '/shaders/primitive.frag',
     DEPTH_TEXTURE_SIZE = 512,
 }
 
