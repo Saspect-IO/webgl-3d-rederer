fc1b4fa4958ae176bb041d3316224a0a4d096775
Sheldon Small <sheldonsmall.io@gmail.com>
2021-07-25 18:43:19 -0500

    update checkeboard to grid

--- src/core/primitives/grid/infinite.ts
+++ src/core/primitives/grid/infinite.ts
@@ -68,12 +68,19 @@ class InfiniteGridShader{
 				return depth;
 			}
 
-			float checkerboard(vec2 R, float scale) {
-				return float((
-					int(floor(R.x / scale)) +
-					int(floor(R.y / scale))
-				) % 2);
-			}
+			
+			vec4 grid(vec3 fragPos3D, float scale) {
+                vec2 coord = fragPos3D.xz * scale;
+                vec2 derivative = fwidth(coord);
+                vec2 grid = abs(fract(coord - 0.5) - 0.5) / derivative;
+                float line = min(grid.x, grid.y);
+                float minimumz = min(derivative.y, 1.0);
+                float minimumx = min(derivative.x, 1.0);
+				vec3 refColor = vec3(47.0, 56.0, 70.0);
+				vec3 gridColor = vec3(1.0 - min(line, 1.0));
+                vec4 color = vec4(mix(refColor, gridColor, 1.0), 1.0);
+                return color;
+            }
 
             out vec4 finalColor;
 
@@ -83,12 +90,12 @@ class InfiniteGridShader{
                 vec3 fragPos3D = nearPoint + t * (farPoint - nearPoint);
 	
 				float isIntersect = (t > 0.0) ? 1.0 : 0.0;
-				float gridPattern = (checkerboard(fragPos3D.xz, 1.0) * 0.3) + (checkerboard(fragPos3D.xz, 10.0) * 0.2) + (checkerboard(fragPos3D.xz, 100.0) * 0.1) + 0.1;
-
 				float linearFieldSection = 0.02 * length(fragPos3D.xz);
 				float fading = min(1.0, 1.5 - linearFieldSection);
 
-				vec4 outColor = vec4(vec3((gridPattern/2.0) + 0.3), 1.0);
+				vec4 gridPattern = grid(fragPos3D, 0.5);
+				
+				vec4 outColor = gridPattern;
 
 				finalColor = outColor * isIntersect;
 				finalColor *= fading;
