6f7c946894464d31d49ea6af8395ad0c46ff394c
Sheldon Small <sheldonsmall.io@gmail.com>
2021-07-28 08:38:25 -0500

    update obj path

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
           loader:'file-loader',
           options:{
             name:'[name].[ext]',
-            outputPath:'assets/resources/formula_1/sources/'
+            outputPath:'assets/resources/formula_1/source/'
           }
         }],
       },
