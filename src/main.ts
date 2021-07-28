6b8732d36e739c0640304398092f18e54566f4e6
SHELDON SMALL <sheldonsmall.io@gmail.com>
2021-07-28 00:18:27 -0500

    Feature/add multiple lighting (#15)
    * update constants
    
    * update lighting and bg color
    
    * refactor code base for depth texture mapping
    
    * disable depth texture

--- src/main.ts
+++ src/main.ts
@@ -7,41 +7,48 @@ import { DirectionalShadow, DirectionalShadowShader } from "./core/shadows/direc
 import { InfiniteGrid, InfiniteGridShader } from "./core/primitives/grid/infinite"
 import { Model, ModelShader } from "./core/model"
 import DepthTexture from "./core/Textures/depthTexture"
+import ObjLoader from "./core/objLoader"
+import Texture from "./core/Textures/texture"
 
 
 (async()=>{
     const glContext = new GLContext(ProgramEntrySettings.WEBGL_CANVAS_ID)
-    glContext.fitScreen(0.95, 0.90).setClearColor(13, 17, 23, 1.0).clear()
+    const [wp, wh] = [0.95, 0.90]
+    glContext.fitScreen(wp, wh).setClearColor(13, 17, 23, 1.0).clear()
     const gl = glContext.getContext() as WebGLRenderingContext
 
-    const lightPosition = new Vector3(1.5, 2.5, 3)
-    const light = new Light(lightPosition)
-    const lightView = new Camera(gl as WebGLRenderingContext)
-    lightView.transform.position.set(lightPosition.x, lightPosition.y, lightPosition.z)
+    const lightViewPosition = new Vector3(0, 2.5, 3)
+    const lightView = new Light(lightViewPosition)
 
-    const directionalShadowShader = new DirectionalShadowShader(gl as WebGLRenderingContext, lightView)
-    const directionalShadow = await DirectionalShadow.createGeometry(gl, directionalShadowShader, PATH_ASSETS_OBJ)
+    const lightCameraView = new Camera(gl as WebGLRenderingContext)
+    lightCameraView.transform.position.set(lightViewPosition.x, lightViewPosition.y, lightViewPosition.z)
+
+    const vertices = await ObjLoader.loadOBJ(PATH_ASSETS_OBJ)
+    const texture = await Texture.loadTexture(gl, PATH_ASSETS_TEXTURE)
+    //const depthTexture = new DepthTexture(gl, ProgramEntrySettings.DEPTH_TEXTURE_SIZE)
+
+    const directionalShadowShader = new DirectionalShadowShader(gl as WebGLRenderingContext, lightCameraView)
+    const directionalShadow = DirectionalShadow.createGeometry(gl, directionalShadowShader, vertices)
     
     const camera = new Camera(gl as WebGLRenderingContext)
     camera.transform.position.set(0, 0.5, 1.5)
     new CameraController(gl as WebGLRenderingContext, camera)
 
-    const infiniteGridShader = new InfiniteGridShader(gl as WebGLRenderingContext, camera, lightView)
-    const infiniteGrid = await InfiniteGrid.createGeometry(gl, infiniteGridShader)
+    const infiniteGridShader = new InfiniteGridShader(gl as WebGLRenderingContext, camera, lightCameraView)
+    const infiniteGrid = InfiniteGrid.createGeometry(gl, infiniteGridShader)
 
-    const modelShader = new ModelShader(gl as WebGLRenderingContext, camera, lightView)
-    const model = await Model.createGeometry(gl, modelShader, PATH_ASSETS_OBJ, PATH_ASSETS_TEXTURE)
+    const modelShader = new ModelShader(gl as WebGLRenderingContext, camera, lightCameraView)
+    const model = Model.createGeometry(gl, modelShader, vertices, texture)
     model.setScale(0.0035,0.0035,0.0035).setRotation(0,30,0)
 
     const loop = () => {
-        
-        glContext.depthRender(directionalShadow.mesh.depth as DepthTexture)
-
-        lightView.updateViewMatrix()
+        lightCameraView.updateViewMatrix()
+        // rendering scene depth
+        //glContext.depthRender(depthTexture)
         directionalShadowShader.setUniforms(gl, directionalShadow.preRender()).shaderProgram
-            .renderModel(directionalShadow.preRender() )
-    
-        glContext.clearFramebuffer().fitScreen(0.95, 0.90).clear()
+            .renderModel(directionalShadow.preRender())
+
+        glContext.clearFramebuffer().fitScreen(wp, wh).clear()
 
         camera.updateViewMatrix()
 
@@ -51,7 +58,7 @@ import DepthTexture from "./core/Textures/depthTexture"
         modelShader.setUniforms(gl, model.preRender())
             .shaderProgram.renderModel(model.preRender())
 
-        light.setUniforms(gl, modelShader, camera)
+        lightView.setUniforms(gl, modelShader)
 
         requestAnimationFrame(loop)
     }
