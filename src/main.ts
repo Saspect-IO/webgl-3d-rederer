bc05db3cd488512b7f13ca278dc3b288a67fa2ba
Sheldon Small <sheldonsmall.io@gmail.com>
2021-07-30 19:49:03 -0500

    revert depth texture

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import { Camera, CameraController } from "./core/camera"
 import GLContext from "./core/glContext"
 import Light from "./core/light"
 import { Vector3 } from "./core/math"
-import { DirectionalShadow, DirectionalShadowShader } from "./core/shadows/directional"
 import { InfiniteGrid, InfiniteGridShader } from "./core/primitives/grid/infinite"
 import { Model, ModelShader } from "./core/model"
-import DepthTexture from "./core/Textures/depthTexture"
 import ObjLoader from "./core/objLoader"
 import Texture from "./core/Textures/texture"
 
@@ -19,16 +17,11 @@ import Texture from "./core/Textures/texture"
 
     const lightViewPosition = new Vector3(0, 2.5, 3)
     const lightView = new Light(lightViewPosition)
-
     const lightCameraView = new Camera(gl as WebGLRenderingContext)
     lightCameraView.transform.position.set(lightViewPosition.x, lightViewPosition.y, lightViewPosition.z)
 
     const vertices = await ObjLoader.loadOBJ(PATH_ASSETS_OBJ)
     const texture = await Texture.loadTexture(gl, PATH_ASSETS_TEXTURE)
-    //const depthTexture = new DepthTexture(gl, ProgramEntrySettings.DEPTH_TEXTURE_SIZE)
-
-    const directionalShadowShader = new DirectionalShadowShader(gl as WebGLRenderingContext, lightCameraView)
-    const directionalShadow = DirectionalShadow.createGeometry(gl, directionalShadowShader, vertices)
     
     const camera = new Camera(gl as WebGLRenderingContext)
     camera.transform.position.set(0, 0.5, 1.5)
@@ -42,13 +35,6 @@ import Texture from "./core/Textures/texture"
     model.setScale(0.0035,0.0035,0.0035).setRotation(0,30,0)
 
     const loop = () => {
-        lightCameraView.updateViewMatrix()
-        // rendering scene depth
-        //glContext.depthRender(depthTexture)
-        directionalShadowShader.setUniforms(gl, directionalShadow.preRender()).shaderProgram
-            .renderModel(directionalShadow.preRender())
-
-        glContext.clearFramebuffer().fitScreen(wp, wh).clear()
 
         camera.updateViewMatrix()
 
