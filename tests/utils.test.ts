dd6737025002f85183a3ec9df76b9ccd45a5e8d6
Sheldon Small <sheldonsmall.io@gmail.com>
2021-08-15 15:07:26 -0500

    update method normalizeRGB, add normalize and lerp methods, update test

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,5 @@
 
-import { normalizeColor } from '../src/modules/utils';
+import { normalizeRGB } from '../src/modules/utils';
 
 import { expect } from 'chai';
 
@@ -7,26 +7,20 @@ import { expect } from 'chai';
 describe('normalizeColor tests', () => {
     it('checking normalizeColor output', () => { 
 
-        let color = normalizeColor({red:255, green:255, blue:255});
-
+        let color = normalizeRGB({red:255, green:255, blue:255});
         expect(color[0]).to.equal(1)
         expect(color[1]).to.equal(1)
         expect(color[2]).to.equal(1)
         expect(color[3]).to.equal(1)
 
-        color = normalizeColor({red:256, green:256, blue:256});
+        const errorMessage = 'band out of range each value must be from 1 - 255'
+        
+        const green = 256
+        expect(()=>{normalizeRGB({red:1, green, blue:1})}).to.throw(`value ${green} for green ${errorMessage}`);
 
-        expect(color[0]).to.equal(1)
-        expect(color[1]).to.equal(1)
-        expect(color[2]).to.equal(1)
-        expect(color[3]).to.equal(1)
+        const red = 0
+        expect(()=>{normalizeRGB({red, green:1, blue:1})}).to.throw(`value ${red} for red ${errorMessage}`);
 
-        color = normalizeColor({red:0, green:0, blue:0});
-
-        expect(color[0]).to.equal(0)
-        expect(color[1]).to.equal(0)
-        expect(color[2]).to.equal(0)
-        expect(color[3]).to.equal(0)
     });
 
 });
