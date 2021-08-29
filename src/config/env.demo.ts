b78ee581223ec8bab6150fd60e9d21a1bd66203b
Sheldon Small <sheldonsmall.io@gmail.com>
2021-08-29 00:02:46 -0500

    update path

--- src/config/env.demo.ts
+++ src/config/env.demo.ts
@@ -1,7 +1,7 @@
 const texture0 = '/webgl-3d-renderer/assets/resources/formula_1/textures/formula_1.png';
-const texture1 = '/webgl-3d-renderer/assets/resources/formula_1/textures/formula1_DefaultMaterial_Specular.png';
-const texture2 = '/webgl-3d-renderer/assets/resources/formula_1/textures/formula1_DefaultMaterial_Glossiness.png';
-const texture3 = '/webgl-3d-renderer/assets/resources/formula_1/textures/formula1_DefaultMaterial_Height.png';
+const texture1 = '/webgl-3d-renderer/assets/resources/formula1_DefaultMaterial_Specular/textures/formula1_DefaultMaterial_Specular.png';
+const texture2 = '/webgl-3d-renderer/assets/resources/formula1_DefaultMaterial_Glossiness/textures/formula1_DefaultMaterial_Glossiness.png';
+const texture3 = '/webgl-3d-renderer/assets/resources/formula1_DefaultMaterial_Height/textures/formula1_DefaultMaterial_Height.png';
 const vertices = '/webgl-3d-renderer/assets/resources/formula_1/source/formula_1.obj';
 
 export default {
