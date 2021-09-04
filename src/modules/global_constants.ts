309a2c61993997d2a7bd43855fcf918995f9b1e9
Sheldon Small <sheldonsmall.io@gmail.com>
2021-09-04 17:19:11 -0500

    remove unused asset paths

--- src/modules/global_constants.ts
+++ src/modules/global_constants.ts
@@ -12,8 +12,6 @@ export enum ProgramEntrySettings {
     WEBGL_DEPTH_TEXTURE = 'WEBGL_depth_texture',
     WEBGL_CONTEXT_ERROR_MESSAGE = 'Could not initialise WebGL',
     WEBGL_DEPTH_TEXTUR_ERROR_MESSAGE = 'need WEBGL_depth_texture',
-    PATH_ASSETS_OBJ = '/webgl-3d-renderer/assets/resources/formula_1/source/formula_1.obj',
-    PATH_ASSETS_TEXTURE = '/webgl-3d-renderer/assets/resources/formula_1/textures/formula_1.png',
     DEPTH_TEXTURE_SIZE = 512,
 }
 
